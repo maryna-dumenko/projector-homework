e96a3cdb142c276d48c6285befaa1540d10cd574
Maryna Dumenko <m.kushneruk.adamivna@gmail.com>
2023-10-16 17:38:43 +0300

    fix function byProperty

--- homework3/thirdTask.js
+++ homework3/thirdTask.js
@@ -29,31 +29,16 @@ console.log(movies.sort(byProperty('releaseYear', '>')));
 // виведе масив фільмів посортованих по року випуску, від старішого до новішого
 console.log(movies.sort(byProperty('runningTimeInMinutes', '<')));
 // виведе масив фільмів посортованих по їх тривалості, від найдовшого до найкоротшого
-console.log(movies.sort(byProperty('movieName', '<')));
+console.log(movies.sort(byProperty('movieName', '>')));
 // виведе масив фільмів посортованих по назві, в алфавітному порядку
 
 function byProperty(property, direction) {
     return function (a, b) {
         if (direction === ">") {
-            if(typeof(a[property]) === "string") {
-                if (a[property] < b[property]) {
-                    return -1
-                } else {
-                    return 1;
-                }
-            } else {
-                return a[property] - b[property];
-            }
+            return (a[property] < b[property]) ? -1 :  1;
+
         } else {
-            if(typeof(a[property]) === "string") {
-                if (a[property] > b[property]) {
-                    return -1
-                } else {
-                    return 1;
-                }
-            } else {
-                return b[property] - a[property];
-            }
+            return (a[property] > b[property]) ? -1 : 1;
         }
     }
 }
