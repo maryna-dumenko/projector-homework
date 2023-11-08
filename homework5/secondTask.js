4eef052a5bab4676ea12d9daa18ca0c4658013a7
Maryna Dumenko <m.kushneruk.adamivna@gmail.com>
2023-11-08 19:44:09 +0200

    add IIFE

--- homework5/secondTask.js
+++ homework5/secondTask.js
@@ -1,41 +1,43 @@
-const buttonToggle = document.querySelector(".button--toggle");
-const container = document.querySelector(".section");
-const infoMessage = document.querySelector(".info-message")
+(function (){
+    const buttonToggle = document.querySelector(".button--toggle");
+    const container = document.querySelector(".section");
+    const infoMessage = document.querySelector(".info-message")
 
-function getPageStateFromStorage() {
-    return JSON.parse(localStorage.getItem('pageState')) || {
-        isTurnedOn: false,
-        lastTimeTurnedOn: null,
-        lastTimeTurnedOff: null
-    };
-}
-
-buttonToggle.addEventListener("click", function (){
-    updateState(getPageStateFromStorage(), new Date());
-})
-
-function updatePage(state) {
-    buttonToggle.innerText = state.isTurnedOn ? "Turn on" : "Turn off";
-    if (state.isTurnedOn) {
-        container.classList.add("section--dark");
-        infoMessage.innerText = state.lastTimeTurnedOn ? `Last turn off: ${state.lastTimeTurnedOn}` : "";
-    } else {
-        container.classList.remove("section--dark");
-        infoMessage.innerText =  state.lastTimeTurnedOff ? `Last turn on: ${state.lastTimeTurnedOff}`: "";
+    function getPageStateFromStorage() {
+        return JSON.parse(localStorage.getItem('pageState')) || {
+            isTurnedOn: false,
+            lastTimeTurnedOn: null,
+            lastTimeTurnedOff: null
+        };
     }
-}
 
-function getDateFormatted(date) {
-    return `${date.getDate()}-${date.getMonth()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-}
+    buttonToggle.addEventListener("click", function (){
+        updateState(getPageStateFromStorage(), new Date());
+    })
 
-function updateState(state, date) {
-    state.lastTimeTurnedOff = state.isTurnedOn? getDateFormatted(date) : state.lastTimeTurnedOff;
-    state.lastTimeTurnedOn = !state.isTurnedOn? getDateFormatted(date) : state.lastTimeTurnedOn;
-    state.isTurnedOn = !state.isTurnedOn;
+    function updatePage(state) {
+        buttonToggle.innerText = state.isTurnedOn ? "Turn on" : "Turn off";
+        if (state.isTurnedOn) {
+            container.classList.add("section--dark");
+            infoMessage.innerText = state.lastTimeTurnedOn ? `Last turn off: ${state.lastTimeTurnedOn}` : "";
+        } else {
+            container.classList.remove("section--dark");
+            infoMessage.innerText =  state.lastTimeTurnedOff ? `Last turn on: ${state.lastTimeTurnedOff}`: "";
+        }
+    }
 
-    localStorage.setItem('pageState', JSON.stringify(state));
-    updatePage(state);
-}
+    function getDateFormatted(date) {
+        return `${date.getDate()}-${date.getMonth()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    }
 
-updatePage(getPageStateFromStorage());
+    function updateState(state, date) {
+        state.lastTimeTurnedOff = state.isTurnedOn? getDateFormatted(date) : state.lastTimeTurnedOff;
+        state.lastTimeTurnedOn = !state.isTurnedOn? getDateFormatted(date) : state.lastTimeTurnedOn;
+        state.isTurnedOn = !state.isTurnedOn;
+
+        localStorage.setItem('pageState', JSON.stringify(state));
+        updatePage(state);
+    }
+
+    updatePage(getPageStateFromStorage());
+})();
