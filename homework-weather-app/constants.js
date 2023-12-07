a6e9e313de6b759fb26053675a049dbb1a497466
Maryna Dumenko <m.kushneruk.adamivna@gmail.com>
2023-12-07 18:01:45 +0200

    [dm] changes fetch to async await

---
+++ homework-weather-app/constants.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('.form');
+const input = document.querySelector('#input');
+const list = document.querySelector('.cities');
+const msg = document.querySelector('.msg');
+const API_KEY = 'f6a1705bc7c080d0511c50fb636e3347';
+
+export {form, input, list, msg, API_KEY};
